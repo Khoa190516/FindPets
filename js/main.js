2016030a1a756244632972008f509a11b6a9329f
Huynh The Khoa <lilo190516@gmail.com>
2023-09-24 10:38:35 +0700

    fix log in

--- js/main.js
+++ js/main.js
@@ -43,8 +43,19 @@ function signOut() {
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
       console.log('User signed out.');
+	  alert("User signed out.");
     });
 }
+
+function onSignIn(googleUser) {
+	var profile = googleUser.getBasicProfile();
+	console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+	console.log('Name: ' + profile.getName());
+	console.log('Image URL: ' + profile.getImageUrl());
+	console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+	alert("Hello: " + profile.getName());
+}
+
 function showPosts(posts) {
 	const featureItems = document.getElementById('posts-list');
 	featureItems.innerHTML = '';
@@ -81,13 +92,4 @@ function showPosts(posts) {
 		`;
 		featureItems.appendChild(postEle);
 	});
-
-	function onSignIn(googleUser) {
-		var profile = googleUser.getBasicProfile();
-		console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-		console.log('Name: ' + profile.getName());
-		console.log('Image URL: ' + profile.getImageUrl());
-		console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-		alert("Hello: " + profile.getName());
-	  }
 }
