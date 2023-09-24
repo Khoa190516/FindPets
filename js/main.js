8f0fd6f05709d886a00d3a38f9f3d9f2f6307977
Huynh The Khoa <lilo190516@gmail.com>
2023-09-24 10:34:39 +0700

    log out

--- js/main.js
+++ js/main.js
@@ -29,7 +29,7 @@ $(document).ready(function () {
 	});
 });
 
-fetchPosts();
+//fetchPosts();
 
 async function fetchPosts() {
 	var response = await fetch('https://localhost:7217/api/Posts');
@@ -39,6 +39,12 @@ async function fetchPosts() {
 	showPosts(posts);
 }
 
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+      console.log('User signed out.');
+    });
+}
 function showPosts(posts) {
 	const featureItems = document.getElementById('posts-list');
 	featureItems.innerHTML = '';
